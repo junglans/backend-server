b2502d0a56f0e8aae70b612137e418c9763a0d9e
jf.jimenez <jf.jimenez@mnemo.es>
2018-06-05 12:11:01 +0200

    Completado servicio de carga de imagenes

--- routes/images.js
+++ routes/images.js
@@ -1,15 +1,23 @@
 var express = require('express');
 var HTTP_OK = require('../lib/constants').HTTP_OK;
+var fs = require('fs');
+var path = require('path');
 
 var app = express();
 
 app.get('/:type/:image', (req, res, next) => {
 
 
-    res.status(HTTP_OK).json({
-        ok: true,
-        message: "Petición correcta - images"
-    });
+    const type = req.params.type;
+    const image = req.params.image;
+
+    const imagePath = path.resolve(__dirname, `../uploads/${type}/${image}`);
+
+    if (fs.existsSync(imagePath)) {
+        res.sendFile(imagePath);
+    } else {
+        res.sendFile(path.resolve(__dirname, '../assets/no-img.png'));
+    }
 
 });
 
